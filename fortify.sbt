5aeff5c972f4100bb1774d816fed0f678ec272b8
Seth Tisue <seth@tisue.net>
2019-12-10 13:46:39 -0800

    add Fortify config

---
+++ fortify.sbt
@@ -0,0 +1,16 @@
+// this makes it so sbt can resolve the plugin
+credentials += Credentials(
+  Path.userHome / ".lightbend" / "commercial.credentials")
+resolvers += "lightbend-commercial-releases" at
+  "https://repo.lightbend.com/commercial-releases/"
+
+// enable the plugin
+addCompilerPlugin(
+  "com.lightbend" %% "scala-fortify" % "1.0.16"
+    classifier "assembly" cross CrossVersion.patch)
+
+// configure the plugin
+scalacOptions ++= Seq(
+  "-P:fortify:scaversion=19.1",
+  "-P:fortify:build=akka-http-webgoat"
+)
---
+++ jenkins.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# this is a copy-and-paste-job from the play-webgoat repo
+
+set -e
+set -v
+
+# let's just assume that ~/.lightbend/license is already in place
+# on CI, it's there because the scala-fortify job left it there.
+# bit janky to have the dependency between the jobs, but meh.
+
+if [[ $(uname -s) = "Darwin" ]]; then
+   SCA_DIR=/Applications/Fortify
+   SBT=/usr/local/bin/sbt
+else
+   SCA_DIR=/home/ubuntu/Fortify
+   SBT=/usr/bin/sbt
+fi
+SOURCEANALYZER=$SCA_DIR/Fortify_SCA_and_Apps_19.1.0/bin/sourceanalyzer
+$SOURCEANALYZER -b akka-http-webgoat -clean
+
+java -version
+
+export JAVA_OPTS=-Dsbt.log.noformat=true
+$SBT clean compile
+
+rm -f target/vulnerabilities-actual.txt
+$SOURCEANALYZER \
+  -b akka-http-webgoat \
+  -logfile target/scan.log \
+  -scan \
+  | tail -n +4 > target/vulnerabilities-actual.txt
+
+cat target/scan.log
+
+diff -u vulnerabilities.txt target/vulnerabilities-actual.txt
+echo SUCCESS
